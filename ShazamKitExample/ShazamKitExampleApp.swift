452ba5b8050cffee00a923e152348dff79829848
Octavianus <oktagandajaya@gmail.com>
2023-05-04 11:47:21 +0700

    Initial Commit

---
+++ ShazamKitExample/ShazamKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamKitExampleApp.swift
+//  ShazamKitExample
+//
+//  Created by octavianus on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ShazamKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
